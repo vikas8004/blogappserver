a537757028b0a8742a309e4f4ad60aa1325da01d
vikas <vikas80046@gmail.com>
2024-06-18 17:07:21 +0530

    added cors

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://blogwave123.netlify.app"],
     credentials: true,
   })
 );
